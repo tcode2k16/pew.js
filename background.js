2168f18732b2d4a920fb975e8a373296ea131d4f
tcode2k16 <tcode2k16@gmail.com>
2016-09-25 17:25:18 +0800

    the first working version

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
     file: 'index.js'
+  }, function (timer) {
+    setTimeout(function () {
+      chrome.tabs.remove(tab.id);
+    },timer);
   });
 });
